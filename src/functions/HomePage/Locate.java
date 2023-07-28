1103dad44103cab8c406418d58d25a358dd84ab0
Guy R <Guy.Roban@Gmail.Com>
2023-07-28 17:45:30 +0300

    initial

---
+++ src/functions/HomePage/Locate.java
@@ -0,0 +1,7 @@
+package HomePage;
+
+import org.openqa.selenium.By;
+
+public class Locate {
+    By btn_SearchHotel = By.xpath("//a[@href='/Hotels']");
+}
--- src/tests/HotelSearch/A_sanityHotelSearch.java
+++ src/tests/HotelSearch/A_sanityHotelSearch.java
@@ -1,2 +1,30 @@
+import HomePage.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class A_sanityHotelSearch {
-}
+    WebDriver driver;
+    HomePage homepage;
+    @BeforeClass
+    public void beforeClass(){
+        driver = new ChromeDriver();
+        homepage = new HomePage(driver);
+        driver.get("https://www.tripadvisor.com/");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    @Test
+    public void searchHotel(){
+        homepage.SearchHotel("Leonardo Privilege Hotel Eilat");
+    }
+
+    }
